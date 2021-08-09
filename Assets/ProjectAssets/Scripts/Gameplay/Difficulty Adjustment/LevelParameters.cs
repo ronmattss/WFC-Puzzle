2f88c66573102e4d52fbf8691f245b979eee58ce
ronmattss <31378489+ronmattss@users.noreply.github.com>
2021-08-09 20:14:04 +0800

    test 3

--- Assets/ProjectAssets/Scripts/Gameplay/Difficulty Adjustment/LevelParameters.cs
+++ Assets/ProjectAssets/Scripts/Gameplay/Difficulty Adjustment/LevelParameters.cs
@@ -4,16 +4,18 @@
     {
         
         
-        private int boardSize = 4;    // set to 4 as default
+        private int boardSize = 4; //BS    // set to 4 as default
 
-        private int expectedMoves = 4; // Computation 1<= EM <= (Board Size – 2)
-        private double allocatedTime = 20;
-        private double boardTime; // Computation
-        private double ExpectedScore; // Computation
+        private int expectedMoves = 4; //EM // Computation 1<= EM <= (Board Size – 2)
+        
+        private double allocatedTime = 20; // AT // (BT * (Expected moves / 10)) + BT
+        private double boardTime; //BT Computation boardSize * 5
+        
+        private double ExpectedScore; // Computation ((EM/PM) * 100% * 60%) + ((70% of AT * SP(70%)) / (RT * PS)) * 40%
         private double completionScore; // Computation EM/PM *100%
         private double timeCompletionScore; // Computation (RT * SP)/(70% of AT * SP(70%)) 
 
-        private double levelRating; // Computation
+        private double levelRating; // Computation EM /(70% of AT * 70%)*100 // NOTE subject to change
         // handles the parameters
         
         // increment board size when the allowed EM is achieved??????
