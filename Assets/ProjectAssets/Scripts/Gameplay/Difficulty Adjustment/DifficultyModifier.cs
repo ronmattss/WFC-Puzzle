692bc7ecee928ab8e8744c0a49c6eb8ce7972928
ronmattss <31378489+ronmattss@users.noreply.github.com>
2021-08-16 23:26:05 +0800

    DDA INITIAL COMPUTATION PARTIALLY DONE

--- Assets/ProjectAssets/Scripts/Gameplay/Difficulty Adjustment/DifficultyModifier.cs
+++ Assets/ProjectAssets/Scripts/Gameplay/Difficulty Adjustment/DifficultyModifier.cs
@@ -10,6 +10,7 @@ namespace ProjectAssets.Scripts.Gameplay.Difficulty_Adjustment
         // level parameters: width height, expected moves, time, 
         public PlayerProfile currentPlayer;
         public LevelDetails levelGenerated;
+        public LevelParameters parameters = new LevelParameters();
 
         void Awake()
         {
@@ -22,13 +23,36 @@ namespace ProjectAssets.Scripts.Gameplay.Difficulty_Adjustment
             
         }
 
+       public void SetupDifficultyParameters()
+        {
+            // Setup Board Size
+            parameters.SetBoardSize(4); // Always the initial BoardSize
+          var time = parameters.SetAllocatedTime();
+            parameters.SetExpectedMoves();
+            parameters.SetExpectedScore(parameters.expectedMoves,time); // Expected Score and TIME
+            var puzzleRating = parameters.SetPuzzleRating();
+            
+            Debug.Log($"Puzzle Rating: {puzzleRating}");
+            // now create the alloted Time
+
+
+
+
+        }
+
         void CalculateLevelRating()
         {
             var currentPlayerRating = currentPlayer.currentRating;
             
+            
+            // first randomly create a level Rating
+            // compare playerRating to the level Rating
+            // if is not in range reduce or add level rating 
 
 
         }
+        
+        
         
         
         
--- Assets/ProjectAssets/Scripts/Gameplay/Difficulty Adjustment/LevelParameters.cs
+++ Assets/ProjectAssets/Scripts/Gameplay/Difficulty Adjustment/LevelParameters.cs
@@ -8,12 +8,12 @@ namespace ProjectAssets.Scripts.Gameplay.Difficulty_Adjustment
         
         private int boardSize = 4; //BS    // set to 4 as default
 
-        private int expectedMoves = 4; //EM // Computation 1<= EM <= (Board Size – 2)
+        public int expectedMoves = 4; //EM // Computation 1<= EM <= (Board Size – 2)
         
         private double allocatedTime = 20; // AT // (BT * (Expected moves / 10)) + BT
         private double boardTime; //BT Computation boardSize * 5
         
-        private double expectedScore; // Computation ((EM/PM) * 100% * 60%) + ((70% of AT * SP(70%)) / (RT * PS)) * 40%
+        public double expectedScore; // Computation ((EM/PM) * 100% * 60%) + ((70% of AT * SP(70%)) / (RT * PS)) * 40%
         private double completionScore; // Computation EM/PM *100%
         private double timeCompletionScore; // Computation (RT * SP)/(70% of AT * SP(70%)) 
 
@@ -21,44 +21,28 @@ namespace ProjectAssets.Scripts.Gameplay.Difficulty_Adjustment
         // handles the parameters
         
         // increment board size when the allowed EM is achieved??????
-        int BoardSize() => boardSize; // should be square n*n
-        double SetBoardTime() => boardSize * 5;
-        double SetAllocatedTime() => (SetBoardTime() * ((double) expectedMoves / 10)) + boardTime;
-        
+        public double SetBoardTime() => boardSize * 5;
         double SetCompletionScore(int playerMovement) => (double) expectedMoves / playerMovement;
+       public double SetPuzzleRating() => expectedMoves / ((.7 * SetAllocatedTime() * .7)) * 100;
 
-        double SetTimeCompletionScore(double remainingTime)
+        public int SetBoardSize(int size)
         {
-            var firstEquation = (remainingTime * .7);
-            var secondEquation = ((SetAllocatedTime() * .7) * .7); // outer .7 is Score Percentage (SP)
-            return firstEquation / secondEquation;
+            boardSize = size;
+            return boardSize;
         }
-        double SetExpectedScore(int playerMovement,double remainingTime)
-        {
-            var firstEquation =  (SetCompletionScore(playerMovement) * .6);
-            var secondEquation = (SetTimeCompletionScore(remainingTime) * .4);
-            expectedScore = ((firstEquation + secondEquation) * 100) / 10;
-            return ((firstEquation + secondEquation) * 100) / 10;
-            // var thirdEquation = (remainingTime *)
-        }
+        // should be square n*n // will be used in level generator, EM should be 
 
-
-        void SetScorePercentage()
+        public double SetAllocatedTime()
         {
-            var scorePercentageRaw = expectedScore;
-            // compute the players score to the expected score
-            // check if the score falls DO i eed this???
-            
+        allocatedTime = (SetBoardTime() * ((double) expectedMoves / 10)) + boardTime;
+        return allocatedTime;
 
         }
         
-
-        int SetExpectedMoves(int add)
+        public int SetExpectedMoves(int add = 0)
         {
             int EM = boardSize; // set the EM to the board size
             int maxRange = boardSize * boardSize;
-            
-            
             // check the current EM
             if(boardSize <= EM && EM < (maxRange - 2))
             {
@@ -70,6 +54,41 @@ namespace ProjectAssets.Scripts.Gameplay.Difficulty_Adjustment
             return EM;
 
         }
+
+        public double SetTimeCompletionScore(double remainingTime)
+        {
+            var firstEquation = (remainingTime * .7);
+            var secondEquation = ((SetAllocatedTime() * .7) * .7); // outer .7 is Score Percentage (SP) // but this is static I guess
+            return firstEquation / secondEquation;
+        }
+        // used to compute for the Expected Score and player Score
+        // used twice, to determine the Level's Score and to Compute the Player's Score
+        public double SetExpectedScore(int playerMovement,double remainingTime)
+        {
+            var firstEquation =  (SetCompletionScore(playerMovement) * .6);
+            var secondEquation = (SetTimeCompletionScore(remainingTime) * .4);
+            expectedScore = ((firstEquation + secondEquation) * 100) / 10;
+            return ((firstEquation + secondEquation) * 100) / 10;
+            // var thirdEquation = (remainingTime *)
+        }
+
+        
+
+        // will be used in the level generator 
+ 
+
+        // what to do first
+        // determine the board size
+        // then The Expected Moves
+        // then the Time
+        // then the Score
+        // then the 2nd Score
+        // then Rating
+        
+
+
+        
+
         
         
         
