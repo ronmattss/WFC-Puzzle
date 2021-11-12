7fefac41ab7db6822dd3e4118dd54914de90db88
ronmattss <31378489+ronmattss@users.noreply.github.com>
2021-11-12 20:07:20 +0800

    tools

--- Assets/ProjectAssets/Scripts/Tools/GenerateEmptyBoard.cs
+++ Assets/ProjectAssets/Scripts/Tools/GenerateEmptyBoard.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
-
+﻿using System;
+using UnityEngine;
+using TMPro;
+using ProjectAssets.Scripts.Gameplay.Difficulty_Adjustment;
 namespace ProjectAssets.Scripts.Tools
 {
     public class GenerateEmptyBoard : MonoBehaviour
@@ -8,7 +10,45 @@ namespace ProjectAssets.Scripts.Tools
         // contains board generation code
         
         
+        public Gameplay.Difficulty_Adjustment.Fuzzy fuzzy;
+        [Header("Tool Simulation Parameters")]
+
+        public int boardSize; 
+        public int expectedMoves; 
+        public int playerMoves; 
+        public int allottedTime; 
+        public float timeLeft; 
+        [Header("Tool Simulation Parameters")]
+       public TMP_Text movesLeftText; 
+       public TMP_Text timeLeftText; 
+       public TMP_Text allottedTimeText; 
+       public TMP_Text playerMovesText; 
+
+
         
         
+        private void FuzzyBasedMoves(int expectedMoves,float allottedTime)
+        {   fuzzy = new Gameplay.Difficulty_Adjustment.Fuzzy();
+            fuzzy.SetMoves(expectedMoves);
+            fuzzy.SetTime(allottedTime);
+            fuzzy.SetIncrementalMoves();
+            fuzzy.AddToDatabase();
+            // Debug Stuff
+            // debugMoves = expectedMoves;
+            // debugTime = allottedTime;
+        }
+        
+        private int OutputFuzzyBasedMoves(int pMoves, float pTimeRemaining)
+        {
+
+            playerMoves = pMoves;
+            timeLeft = pTimeRemaining;
+            return Mathf.FloorToInt(fuzzy.AcceptInput(pMoves, pTimeRemaining));
+        }
+
+        public void LateUpdate()
+        {
+            
+        }
     }
 }
